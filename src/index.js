ef06375c154afd01252920d6e6ab0b05cb8e6b09
Gyorgy Zsapka <gyorgy.zsapka@gmail.com>
2021-06-04 16:04:34 +0200

    fix: fix module exports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 'use strict'
 
-const _modules = ['Buyer', 'Client', 'Invoice', 'Item', 'Seller', 'Receipt', 'ReceiptItem']
-
-_modules.forEach((n) => {
-  exports[n] = require('./lib/' + n)
-})
+exports.Buyer = require('./lib/Buyer')
+exports.Client = require('./lib/Client')
+exports.Invoice = require('./lib/Invoice')
+exports.Item = require('./lib/Item')
+exports.Seller = require('./lib/Seller')
+exports.Receipt = require('./lib/Receipt')
+exports.ReceiptItem = require('./lib/ReceiptItem')
 
 require('./lib/Constants').setup(exports)
--- src/lib/Buyer.js
+++ src/lib/Buyer.js
@@ -9,47 +9,50 @@ const defaultOptions = {
 }
 
 class Buyer {
-  constructor (options) {
+  constructor(options) {
     this._options = merge.recursive(true, defaultOptions, options || {})
 
-    assert(typeof this._options.name === 'string' && this._options.name.trim().length > 0,
-      'Valid Name field missing from buyer options')
+    assert(typeof this._options.name === 'string' && this._options.name.trim().length > 0, 'Valid Name field missing from buyer options')
 
-    assert(typeof this._options.zip === 'string',
-      'Zip field missing from buyer options')
+    assert(typeof this._options.zip === 'string', 'Zip field missing from buyer options')
 
-    assert(typeof this._options.city === 'string' && this._options.city.trim().length > 0,
-      'Valid City field missing from buyer options')
+    assert(typeof this._options.city === 'string' && this._options.city.trim().length > 0, 'Valid City field missing from buyer options')
 
-    assert(typeof this._options.address === 'string' && this._options.address.trim().length > 0,
-      'Valid Address field missing from buyer options')
+    assert(
+      typeof this._options.address === 'string' && this._options.address.trim().length > 0,
+      'Valid Address field missing from buyer options'
+    )
   }
 
-  _generateXML (indentLevel) {
+  _generateXML(indentLevel) {
     indentLevel = indentLevel || 0
 
-    return XMLUtils.wrapWithElement('vevo', [
-      [ 'nev', this._options.name ],
-      [ 'orszag', this._options.country ],
-      [ 'irsz', this._options.zip ],
-      [ 'telepules', this._options.city ],
-      [ 'cim', this._options.address ],
-      [ 'email', this._options.email ],
-      [ 'sendEmail', this._options.sendEmail ],
-      [ 'adoszam', this._options.taxNumber ],
-      [ 'adoszamEU', this._options.taxNumberEU ],
-      [ 'postazasiNev', this._options.postAddress.name ],
-      [ 'postazasiOrszag', this._options.postAddress.country ],
-      [ 'postazasiIrsz', this._options.postAddress.zip ],
-      [ 'postazasiTelepules', this._options.postAddress.city ],
-      [ 'postazasiCim', this._options.postAddress.address ],
-      // TODO: The following feature hasn't implemented yet
-      // ['vevoFokonyv', ],
-      [ 'azonosito', this._options.identifier ],
-      [ 'alairoNeve', this._options.issuerName ],
-      [ 'telefonszam', this._options.phone ],
-      [ 'megjegyzes', this._options.comment ]
-    ], indentLevel)
+    return XMLUtils.wrapWithElement(
+      'vevo',
+      [
+        ['nev', this._options.name],
+        ['orszag', this._options.country],
+        ['irsz', this._options.zip],
+        ['telepules', this._options.city],
+        ['cim', this._options.address],
+        ['email', this._options.email],
+        ['sendEmail', this._options.sendEmail],
+        ['adoszam', this._options.taxNumber],
+        ['adoszamEU', this._options.taxNumberEU],
+        ['postazasiNev', this._options.postAddress.name],
+        ['postazasiOrszag', this._options.postAddress.country],
+        ['postazasiIrsz', this._options.postAddress.zip],
+        ['postazasiTelepules', this._options.postAddress.city],
+        ['postazasiCim', this._options.postAddress.address],
+        // TODO: The following feature hasn't implemented yet
+        // ['vevoFokonyv', ],
+        ['azonosito', this._options.identifier],
+        ['alairoNeve', this._options.issuerName],
+        ['telefonszam', this._options.phone],
+        ['megjegyzes', this._options.comment]
+      ],
+      indentLevel
+    )
   }
 }
 
